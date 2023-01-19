674518314d9fe5c735c0e752c2a14ad2881ddf56
Andreas Kiener <andreas.kiener@gmail.com>
2023-01-19 20:30:04 +0100

    Add component MainLayout and use it

--- pages/index.js
+++ pages/index.js
@@ -18,9 +18,7 @@ export default function Home({ data }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
       <HomePage data={data} />
-      <Footer />
     </>
   );
 }
