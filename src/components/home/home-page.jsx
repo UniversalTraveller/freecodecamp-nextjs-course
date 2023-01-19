aaffcc2ec8ab5b0b4073209a5b2f9b39f5ce2800
Andreas Kiener <andreas.kiener@gmail.com>
2023-01-19 20:00:36 +0100

    Change homepage component

--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -1,16 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export const HomePage = ({ data }) => {
-  return (
-    <main>
-      {data.map((ev) => (
-        <Link key={ev.id} href={`/events/${ev.id}`}>
-          <Image alt={ev.title} src={ev.image} width={200} height={200} />
-          <h2>{ev.title}</h2>
-          <p>{ev.description}</p>
-        </Link>
-      ))}
-    </main>
-  );
-};
+export const HomePage = ({ data }) => (
+  <main>
+    {data.map((ev) => (
+      <Link key={ev.id} href={`/events/${ev.id}`}>
+        <Image alt={ev.title} src={ev.image} width={200} height={200} />
+        <h2>{ev.title}</h2>
+        <p>{ev.description}</p>
+      </Link>
+    ))}
+  </main>
+);
