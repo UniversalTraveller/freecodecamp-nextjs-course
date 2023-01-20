d2b5589dae2699aaa53f57a0eee7c46984ba1e9b
Andreas Kiener <andreas.kiener@gmail.com>
2023-01-20 19:13:19 +0100

    Add styling for event registration form

--- src/components/events/single-event.jsx
+++ src/components/events/single-event.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import Image from "next/image";
 
 const SingleEvent = ({ data }) => {
+  const submitHandler = () => {};
   return (
-    <div>
+    <div className="event_single_page">
       <Image src={data.image} width={1000} height={500} alt={data.title} />
       <h1>{data.title}</h1>
       <p>{data.description}</p>
-      <form>
-        {" "}
-        <input type="email" />
-        <button>Submit</button>
+      <form onSubmit={submitHandler} className="email_registration">
+        <label>Get registered for this event! </label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Please enter our email here!"
+        />
+        <button type="button">Submit</button>
       </form>
     </div>
   );
